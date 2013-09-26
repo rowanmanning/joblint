3a1d754fdb80b3eb480f86982e69ecaf3ae313f1
Rowan Manning <accounts@rowanmanning.co.uk>
2013-09-26 23:14:03 +0100

    Make the command-line tool accept stdin or file

--- bin/joblint.js
+++ bin/joblint.js
@@ -1,6 +1,54 @@
 #!/usr/bin/env node
 'use strict';
 
+var fs = require('fs');
 var joblint = require('../lib/joblint');
 
-console.log(joblint('This is some text which is part of a job spec'));
+runCommand();
+
+function runCommand () {
+    if (processHasArgs()) {
+        runCommandOnFile(process.argv[2]);
+    } else {
+        runCommandOnStdIn();
+    }
+}
+
+function runCommandOnFile (fileName) {
+    fs.readFile(fileName, {encoding: 'utf8'}, function (err, data) {
+        if (err) {
+            handleInputFailure('File "' + fileName + '" not found');
+        } else {
+            handleInputSuccess(data);
+        }
+    });
+}
+
+function runCommandOnStdIn () {
+    captureStdIn(handleInputSuccess);
+}
+
+function handleInputFailure (msg) {
+    console.error(msg);
+    process.exit(1);
+}
+
+function handleInputSuccess (data) {
+    var result = joblint(data);
+    console.log(result);
+}
+
+function processHasArgs () {
+    return (process.argv.length > 2);
+}
+
+function captureStdIn (done) {
+    var data = '';
+    process.stdin.resume();
+    process.stdin.on('data', function (chunk) {
+        data += chunk;
+    });
+    process.stdin.on('end', function () {
+        done(data);
+    });
+}
