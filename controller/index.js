0f51f1ea37e0879ac31e2c7de7070def5a696844
Rowan Manning <accounts@rowanmanning.co.uk>
2013-10-01 18:37:46 +0100

    Add functioning form to the home page

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,23 @@
 'use strict';
 
+var joblint = require('joblint');
+
 module.exports = defineController;
 
 function defineController (app) {
     app.get('/', function (req, res) {
         res.render('index');
     });
+    app.post('/', requireUnemptyJobSpec, function (req, res) {
+        res.render('result', {
+            result: joblint(req.body.spec)
+        });
+    });
+}
+
+function requireUnemptyJobSpec (req, res, next) {
+    if (!req.body.spec || !req.body.spec.trim()) {
+        return res.redirect('/');
+    }
+    next();
 }
