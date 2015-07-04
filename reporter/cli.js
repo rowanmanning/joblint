bc885330d086cf4a6e602367c9b9e10ad4349098
Rowan Manning <info@rowanmanning.com>
2015-07-04 10:17:34 +0100

    Add the tally chart to the CLI reporter

--- reporter/cli.js
+++ reporter/cli.js
@@ -1,14 +1,57 @@
 'use strict';
 
 var chalk = require('chalk');
+var pad = require('pad-component');
 var wrap = require('wordwrap')(4, Math.min(process.stdout.columns - 4, 76));
 
 module.exports = report;
 
 function report (result) {
     console.log('\n%s', chalk.cyan.underline('Joblint'));
-    result.issues.forEach(reportIssue);
+    if (Object.keys(result.counts).length) {
+        console.log('\n%s', chalk.grey('Issue tally:'));
+        reportTallyChart(result.counts);
+    }
+    if (result.issues.length) {
+        result.issues.forEach(reportIssue);
+    }
+    else {
+        console.log('\n' + chalk.green('✔ No issues found!'));
+    }
     console.log('');
+}
+
+function reportTallyChart (counts) {
+    var labels = Object.keys(counts);
+    var values = labels.map(function (label) {
+        return counts[label];
+    });
+    var bars = values.map(function (count) {
+        return pad.right('', count, '█');
+    });
+    bars = bars.map(padEach(getLongest(bars)));
+    labels.map(padEach(getLongest(labels))).forEach(function (label, index) {
+        console.log(
+            capitalizeFirstLetter(label),
+            chalk.grey(' |') + chalk.yellow(bars[index]),
+            chalk.grey(' (' + values[index] + ')')
+        );
+    });
+}
+
+function padEach (length, character) {
+    return function (value) {
+        return pad.right(value, length, character);
+    };
+}
+
+function getLongest (array) {
+    return array.reduce(function (longest, current) {
+        if (current.length > longest) {
+            return current.length;
+        }
+        return longest;
+    }, 0);
 }
 
 function reportIssue (issue) {
@@ -34,4 +77,8 @@ function getColorForLevel (level) {
         return 'yellow';
     }
     return 'cyan';
+}
+
+function capitalizeFirstLetter (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
